a85e4e3ceb05bd9481c2ad66b8cd3567cf5418ba
mike <mich.rivera84@gmail.com>
2025-03-24 21:03:37 -0400

    setting up webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const htmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	devtool: "eval-source-map",
+	devServer: {
+		watchFiles: ["./src/template.html"],
+	},
+	plugins: [
+		new htmlWebpackPlugin ({
+			template: "./src/template.html",
+		}),
+	],
+	
+	module: {
+		rules: [
+			{
+				test: /\.css/i,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+}
